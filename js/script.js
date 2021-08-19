27f0f66f22a1f2c133ef44ec771a990992ca7751
Son Hoang <hoangminhson29102001@gmail.com>
2021-08-19 17:35:38 +0700

    responsive body

--- js/script.js
+++ js/script.js
@@ -75,13 +75,86 @@ $(function() {
     }
 
     
+    /*            JUMBOTRON SLIDE          */
+
+    function Jumbotron_slide_show() {
+        let auto_slideIndex = -1;
+        chose_jumbotron_slides();
+        auto_jumbotron_slides();
+
+        function auto_jumbotron_slides() {
+        
+            const slides = $(".jumbotron__slide");
+            const dots = $(".dots");
+            
+            const dots_arr = Array.prototype.slice.call(dots);
+    
+            // we make slide auto by slideIndex run infitive
+            // from 0 - slides.length again and again
+            if(auto_slideIndex >= slides.length - 1 ) {
+                auto_slideIndex = -1;
+            }
+    
+            slides.removeClass("slide--active");
+            dots.removeClass("dots--active");
+            auto_slideIndex ++;
+    
+    
+            $(slides[auto_slideIndex]).addClass("slide--active");
+            $(dots[auto_slideIndex]).addClass("dots--active");
+            setTimeout(auto_jumbotron_slides, 4000);
+        }
+
+
+
+        function chose_jumbotron_slides() {
+
+            const slides = $(".jumbotron__slide");
+            const dots = $(".dots");
+            
+            const dots_arr = Array.prototype.slice.call(dots);
+            
+    
+    
+            // Add avtive when click dot
+            dots.click(function() {
+                 //index of chosen slide = index of chosen dots
+                var currentSlideIndex = dots_arr.indexOf(this);
+    
+    
+                dots.removeClass("dots--active");
+                slides.removeClass("slide--active");
+    
+                
+                $(slides[currentSlideIndex]).addClass("slide--active");
+                $(this).addClass("dots--active");
+            
+            })
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 
     
-    // inputbox.val("off");
-    // console.log(inputbox.val());
+    
+    
+
+
 
 
     open_lan_curr_tooltip();
     open_and_close_navbar_md();
+
+    // chose_jumbotron_slides();
+    // auto_jumbotron_slides();
+    Jumbotron_slide_show();
 
 })
